5f01cc97e697b06330ad932416f98f739c696133
Jacobtread <jacobtread@gmail.com>
2022-08-27 13:38:33 +1200

    Auth scope with path

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -18,6 +18,20 @@ pub fn auth_scope(auth_store: AuthStoreSafe) -> Scope<
         InitError=(),
     >,
 > {
-    Scope::new("")
+    return auth_scope_with_path("", auth_store);
+}
+
+/// Creates a scope that is protected by the auth store
+/// authentication middleware this scope has a path
+pub fn auth_scope_with_path(path: &str, auth_store: AuthStoreSafe) -> Scope<
+    impl ServiceFactory<
+        ServiceRequest,
+        Config=(),
+        Response=ServiceResponse<BoxBody>,
+        Error=Error,
+        InitError=(),
+    >,
+> {
+    Scope::new(path)
         .wrap(AuthMiddleware::new(auth_store))
 }
