9cbb1eb0af76eb8a621007079bee2b0c6b5addc2
Jacobtread <jacobtread@gmail.com>
2022-08-28 14:56:21 +1200

    Restructured styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './assets/styles/App.scss';
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Auth from "./routes/Auth";
