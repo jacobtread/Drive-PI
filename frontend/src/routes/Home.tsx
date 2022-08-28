393978f1d676dcd3dd34972718537e95c4a880cd
Jacobtread <jacobtread@gmail.com>
2022-08-28 17:16:02 +1200

    Home page logout button

--- frontend/src/routes/Home.tsx
+++ frontend/src/routes/Home.tsx
@@ -1,9 +1,14 @@
 import { FunctionComponent } from "react";
+import { useAccess } from "../components/AccessProvider";
 
 const Home: FunctionComponent = () => {
+    const {logout} = useAccess()
     return (
         <div>
             <h1>Home</h1>
+            <button onClick={logout}>
+                Logout
+            </button>
         </div>
     )
 }
