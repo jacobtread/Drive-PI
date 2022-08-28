de7df71749150dab065ffc0d6e7d1bd0f3866bae
Jacobtread <jacobtread@gmail.com>
2022-08-28 17:22:13 +1200

    Cleaned up copy script

--- frontend/copy-output.js
+++ frontend/copy-output.js
@@ -2,32 +2,38 @@ const fs = require("fs/promises");
 const fsSync = require("fs")
 const path = require("path");
 
-copyOutput().then()
-    .catch(console.error)
+// The input directory path (./build)
+const inputPath = path.join(".", "build");
+// The output directory path (../backend/public)
+const outputPath = path.join("..", "backend", "public");
 
-async function copyOutput() {
-    const inputPath = path.join(".", "build");
-
-    if (!fsSync.existsSync(inputPath)) {
-        console.error("Missing input files cannot copy");
-        return
-    }
-
-    const outputPath = path.join("..", "backend", "public");
-
-    if (fsSync.existsSync(outputPath)) {
-        console.log("Removing output directory")
-        await fs.rm(outputPath, {recursive: true});
-    }
-
+// Check the build files actually exist
+if (fsSync.existsSync(inputPath)) {
     console.log("Copying Build Files")
-    await copyFolder(inputPath, outputPath)
+    // Copy the build files
+    copyFolder(inputPath, outputPath)
+        .then(() => console.log("Successfully copied output"))
+        .catch(e => console.error("Failed to copy output", e))
+} else {
+    console.error("Missing input files cannot copy");
 }
 
+
+/**
+ * Recursive copy folder function. Recursively copies all files
+ * and folders from the source path to the target path.
+ *
+ * Will remove the target directory if it exists.
+ *
+ * @param source The source path
+ * @param target The target path
+ * @returns {Promise<void>} Promise indicating the completion of the copy
+ */
 async function copyFolder(source, target) {
-    if (!fsSync.existsSync(target)) {
-        await fs.mkdir(target);
+    if (fsSync.existsSync(target)) {
+        await fs.rm(target, {recursive: true});
     }
+    await fs.mkdir(target);
     const files = await fs.readdir(source)
     const promises = files.map(async (file) => {
         const inPath = path.join(source, file)
