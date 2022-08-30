974c00373c412f0e7ce360b87912b2a6db8d210a
Jacobtread <jacobtread@gmail.com>
2022-08-30 20:06:52 +1200

    Styled file browser

--- frontend/src/components/FileBrowser.tsx
+++ frontend/src/components/FileBrowser.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, useEffect, useState } from "react";
-import drives, { Drive } from "$components/Drives";
+import { Drive } from "$components/Drives";
 import { useAccess } from "$components/AccessProvider";
 
 interface Properties {
@@ -34,28 +34,33 @@ interface DriveState {
 }
 
 const HomeIcon: FunctionComponent = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"fill="#999" className="icon home-icon">
-        <path
-            d="M8 42V18L24.1 6 40 18v24H28.3V27.75h-8.65V42Zm3-3h5.65V24.75H31.3V39H37V19.5L24.1 9.75 11 19.5Zm13-14.65Z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#999"
+         className="icon home-icon">
+        <path d="M0 0h24v24H0V0z" fill="none"/>
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8h5z"/>
     </svg>
 )
 
 const BackIcon: FunctionComponent = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="#999" className="icon home-icon">
-        <path d="M20 44 0 24 20 4l2.8 2.85L5.65 24 22.8 41.15Z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#999"
+         className="icon home-icon">
+        <path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/>
+        <path d="M17.51 3.87L15.73 2.1 5.84 12l9.9 9.9 1.77-1.77L9.38 12l8.13-8.13z"/>
     </svg>
 )
 
 const FolderIcon: FunctionComponent = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="#999" className="icon folder-icon">
-        <path
-            d="M7.05 40q-1.2 0-2.1-.925-.9-.925-.9-2.075V11q0-1.15.9-2.075Q5.85 8 7.05 8h14l3 3h17q1.15 0 2.075.925.925.925.925 2.075v23q0 1.15-.925 2.075Q42.2 40 41.05 40Zm0-29v26h34V14H22.8l-3-3H7.05Zm0 0v26Z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 0 24 24" width="32px" fill="#999"
+         className="icon home-icon">
+        <path d="M0 0h24v24H0V0z" fill="none"/>
+        <path d="M10 4H2v16h20V6H12l-2-2z"/>
     </svg>
 )
 const FileIcon: FunctionComponent = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48" fill="#999" className="icon file-icon">
-        <path
-            d="M15.95 35.5h16.1v-3h-16.1Zm0-8.5h16.1v-3h-16.1ZM11 44q-1.2 0-2.1-.9Q8 42.2 8 41V7q0-1.2.9-2.1Q9.8 4 11 4h18.05L40 14.95V41q0 1.2-.9 2.1-.9.9-2.1.9Zm16.55-27.7V7H11v34h26V16.3ZM11 7v9.3V7v34V7Z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 0 24 24" width="32px" fill="#999"
+         className="icon home-icon">
+        <path d="M0 0h24v24H0V0z" fill="none"/>
+        <path d="M14 2H4v20h16V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"/>
     </svg>
 )
 
@@ -126,14 +131,18 @@ const FileBrowser: FunctionComponent<Properties> = ({drive}) => {
         <div className="browser">
             <div className="browser__path input">
                 <div className="browser__toolbar">
-                    <button onClick={moveHome} disabled={isRoot}>
+                    <button
+                        className="button browser__toolbar__button"
+                        onClick={moveHome} disabled={isRoot}>
                         <HomeIcon/>
                     </button>
-                    <button onClick={moveBack} disabled={isRoot}>
+                    <button
+                        className="button browser__toolbar__button"
+                        onClick={moveBack} disabled={isRoot}>
                         <BackIcon/>
                     </button>
                 </div>
-                <input type="text" readOnly={true} className="browser__path__input input__value"/>
+                <input type="text" readOnly={true} className="browser__path__input input__value" value={path}/>
             </div>
 
             <ul className="browser__list">
@@ -158,7 +167,7 @@ const FileBrowser: FunctionComponent<Properties> = ({drive}) => {
                             <FileIcon/>
                             <div>
                                 <p className="browser__item__name">{file.name}</p>
-                                <span>{file.size} bytes</span>
+                                <span className="browser__item__size">{file.size} bytes</span>
                             </div>
                         </li>
                     )
