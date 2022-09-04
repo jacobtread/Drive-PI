06e533cfa88361d0ac6c9e6f0c50ffb79d58fa85
Jacobtread <jacobtread@gmail.com>
2022-09-05 10:22:26 +1200

    Merged catchall serve for web app

--- backend/src/routes/app.rs
+++ backend/src/routes/app.rs
@@ -2,7 +2,7 @@ use actix_web::{get, HttpResponse, web};
 use actix_web::body::BoxBody;
 use actix_web::http::header::ContentType;
 use mime_guess::from_path;
-use rust_embed::{EmbeddedFile, RustEmbed};
+use rust_embed::{RustEmbed};
 use crate::define_routes;
 
 #[derive(RustEmbed)]
@@ -11,18 +11,15 @@ struct PublicDir;
 
 define_routes!(public);
 
-async fn serve_file(path: &str, file: EmbeddedFile) -> HttpResponse<BoxBody> {
-    HttpResponse::Ok()
-        .content_type(from_path(path)
-            .first_or_octet_stream()
-            .as_ref())
-        .body(file.data.into_owned())
-}
-
 #[get("/{filename:.*}")]
 async fn public(path: web::Path<String>) -> HttpResponse<BoxBody> {
-    if let Some(file) = PublicDir::get(&path) {
-        serve_file(&path, file).await
+    let path: &String = path.as_ref();
+    if let Some(file) = PublicDir::get(path) {
+        HttpResponse::Ok()
+            .content_type(from_path(path)
+                .first_or_octet_stream()
+                .as_ref())
+            .body(file.data.into_owned())
     } else if let Some(file) = PublicDir::get("index.html") {
         HttpResponse::Ok()
             .content_type(ContentType::html())
