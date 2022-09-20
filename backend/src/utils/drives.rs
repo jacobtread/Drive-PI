7c66d526371dad34b84e1a1701d4ee28378799b7
Jacobtread <jacobtread@gmail.com>
2022-09-20 21:30:05 +1200

    Safely wrapping paths and recursive chmod

--- backend/src/utils/drives.rs
+++ backend/src/utils/drives.rs
@@ -86,16 +86,16 @@ pub fn mount_drive(path: &String, name: &String) -> DrivesResultEmpty {
     // Ensure the local mounting root point exists or create it
     let mount_dir = Path::new(MOUNT_DIR);
     if !mount_dir.exists() {
-        fs::create_dir(mount_dir).map_err(|err| {
+        create_dir(mount_dir).map_err(|err| {
             error!("Failed to create mount target parent directory: {}", err);
             DrivesError::MountError
         })?;
     }
 
     // Ensure the local mounting point exists or create it
-    let mount_path = mount_dir.join(name);
+    let mount_path = mount_dir.join(name).canonicalize()?;
     if !mount_path.exists() {
-        fs::create_dir(&mount_path).map_err(|err| {
+        create_dir(&mount_path).map_err(|err| {
             error!("Failed to create mount target directory: {}", err);
             DrivesError::MountError
         })?;
@@ -103,12 +103,15 @@ pub fn mount_drive(path: &String, name: &String) -> DrivesResultEmpty {
 
     let mount_path_str = mount_path.to_str().ok_or_else(|| DrivesError::MountError)?;
 
+    // Wrapping mount path in quotations to prevent errors
+    let mount_path_str = format!("\"{}\"", mount_path_str);
+
     let output = Command::new("mount")
         .args([
             "-o",
             "rw", /* Mount as Read/Write*/
             path,
-            mount_path_str,
+            &mount_path_str,
         ])
         .output()
         .map_err(|err| {
@@ -119,14 +122,13 @@ pub fn mount_drive(path: &String, name: &String) -> DrivesResultEmpty {
     let status = output.status;
 
     if !status.success() {
-        let stderr =
-            String::from_utf8(output.stderr).unwrap_or(String::from("Failed to parse stderr"));
+        let stderr = String::from_utf8_lossy(&output.stderr);
 
         warn!("Failed to mount drive {}", stderr);
 
         Err(DrivesError::MountError)
     } else {
-        chown_mounted_drive(mount_path_str)?;
+        chown_mounted_drive(&mount_path_str)?;
 
         Ok(())
     }
@@ -134,7 +136,11 @@ pub fn mount_drive(path: &String, name: &String) -> DrivesResultEmpty {
 
 fn chown_mounted_drive(path: &str) -> DrivesResultEmpty {
     let output = Command::new("chmod")
-        .args(["a+rw" /* Read/Write*/, path])
+        .args([
+            "-R",   /* Recursive ownership change */
+            "a+rw", /* Read/Write*/
+            path,   /* Safe version of path */
+        ])
         .output()
         .map_err(|err| {
             error!("Failed to execute chmod command: {}", err);
@@ -144,8 +150,7 @@ fn chown_mounted_drive(path: &str) -> DrivesResultEmpty {
     let status = output.status;
 
     if !status.success() {
-        let stderr =
-            String::from_utf8(output.stderr).unwrap_or(String::from("Failed to parse stderr"));
+        let stderr = String::from_utf8_lossy(&output.stderr);
 
         warn!("Failed to change mounted drive permissions {}", stderr);
         Err(DrivesError::MountError)
@@ -179,9 +184,7 @@ pub fn unmount_drive(path: &String, name: &String) -> DrivesResultEmpty {
     let status = output.status;
 
     if !status.success() {
-        let stderr =
-            String::from_utf8(output.stderr).unwrap_or(String::from("Failed to parse stderr"));
-
+        let stderr = String::from_utf8_lossy(&output.stderr);
         if stderr.contains("target is busy") {
             Err(DrivesError::TargetBusy)
         } else {
